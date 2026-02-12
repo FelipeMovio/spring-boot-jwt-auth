b5a0652c361ebdf0abf08f1b49e920a8e5cf0777
FelipeMovio <felipelsmovio@gmail.com>
2026-02-12 19:13:57 -0300

    generate constructors

--- src/main/java/com/autenticaoJWT/demo/model/Usuario.java
+++ src/main/java/com/autenticaoJWT/demo/model/Usuario.java
@@ -1,4 +1,31 @@
 package com.autenticaoJWT.demo.model;
 
+
+import jakarta.persistence.*;
+
+@Entity
 public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    public Usuario(Long id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Usuario() {
+    }
+
+
 }
