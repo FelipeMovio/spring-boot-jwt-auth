778f1349f7af7203e6bddf865e6519ea69fc7a8b
FelipeMovio <felipelsmovio@gmail.com>
2026-02-12 19:21:31 -0300

    finally repository

--- src/main/java/com/autenticaoJWT/demo/repository/UsuarioRepository.java
+++ src/main/java/com/autenticaoJWT/demo/repository/UsuarioRepository.java
@@ -3,5 +3,8 @@ package com.autenticaoJWT.demo.repository;
 import com.autenticaoJWT.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository< Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
 }
