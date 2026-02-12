1be4a39303e8ec99aa12beea36da414d5bfbe531
FelipeMovio <felipelsmovio@gmail.com>
2026-02-12 19:18:08 -0300

    lombok

--- src/main/java/com/autenticaoJWT/demo/model/Usuario.java
+++ src/main/java/com/autenticaoJWT/demo/model/Usuario.java
@@ -2,6 +2,8 @@ package com.autenticaoJWT.demo.model;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.jspecify.annotations.Nullable;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -9,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Usuario implements UserDetails {
 
@@ -31,38 +35,6 @@ public class Usuario implements UserDetails {
     }
 
     public Usuario() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
     }
 
     @Override
