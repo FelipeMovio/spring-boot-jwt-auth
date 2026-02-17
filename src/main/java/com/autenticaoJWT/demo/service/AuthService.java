5c0eb68c6d46ece49d1d77faccafaa57f1919e68
FelipeMovio <felipelsmovio@gmail.com>
2026-02-16 21:14:38 -0300

    ... update

--- src/main/java/com/autenticaoJWT/demo/service/AuthService.java
+++ src/main/java/com/autenticaoJWT/demo/service/AuthService.java
@@ -1,12 +1,19 @@
 package com.autenticaoJWT.demo.service;
 
+import com.autenticaoJWT.demo.model.Usuario;
+import com.autenticaoJWT.demo.repository.UsuarioRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 
 public class AuthService implements UserDetailsService {
 
+    private UsuarioRepository usuarioRepository;
 
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
