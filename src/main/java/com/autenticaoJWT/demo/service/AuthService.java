38c69f02f8de796f58634ded33a7e05cb640b9af
FelipeMovio <felipelsmovio@gmail.com>
2026-02-16 21:15:39 -0300

    instanciando os atributos

--- src/main/java/com/autenticaoJWT/demo/service/AuthService.java
+++ src/main/java/com/autenticaoJWT/demo/service/AuthService.java
@@ -6,14 +6,20 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 
-
+@Service
 public class AuthService implements UserDetailsService {
 
     private UsuarioRepository usuarioRepository;
 
     private PasswordEncoder passwordEncoder;
+
+    public AuthService(UsuarioRepository usuarioRepository, PasswordEncoder passwordEncoder) {
+        this.usuarioRepository = usuarioRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
