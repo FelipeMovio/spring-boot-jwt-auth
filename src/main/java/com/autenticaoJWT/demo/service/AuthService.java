219c9d6c35fc929132e8f54ee1a77076842349ed
FelipeMovio <felipelsmovio@gmail.com>
2026-02-16 21:08:01 -0300

    implements UserDetailsService

--- src/main/java/com/autenticaoJWT/demo/service/AuthService.java
+++ src/main/java/com/autenticaoJWT/demo/service/AuthService.java
@@ -1,4 +1,12 @@
 package com.autenticaoJWT.demo.service;
 
-public class AuthService {
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
