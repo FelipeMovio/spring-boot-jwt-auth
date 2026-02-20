bdd3a49ad52db9bc1b56ce575e9387bf8a5b407f
FelipeMovio <felipelsmovio@gmail.com>
2026-02-20 19:17:44 -0300

    update

--- src/main/java/com/autenticaoJWT/demo/security/TokenConfig.java
+++ src/main/java/com/autenticaoJWT/demo/security/TokenConfig.java
@@ -1,4 +1,27 @@
 package com.autenticaoJWT.demo.security;
 
+import com.autenticaoJWT.demo.model.Usuario;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
 public class TokenConfig {
+
+    private String secret = "secret";
+
+    public String generateToken(Usuario user) {
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withClaim("userId", user.getId())
+                .withClaim("roles", user.getRoles().stream().map(Enum::name).toList())
+                .withSubject(user.getEmail())
+                .withExpiresAt(Instant.now().plusSeconds(86400)) // 24h duração do token
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+    }
+
 }
