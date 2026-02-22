cb0332365fc72872f6af3fe5ce297c1e3681e484
FelipeMovio <felipelsmovio@gmail.com>
2026-02-22 13:05:40 -0300

    admin cria admin somente

---
+++ src/main/java/com/autenticaoJWT/demo/DTO/request/AdminCreateUserRequestDto.java
@@ -0,0 +1,17 @@
+package com.autenticaoJWT.demo.DTO.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AdminCreateUserRequestDto(
+
+        @Email(message = "Email inválido")
+        @NotBlank(message = "Email é obrigatório")
+        String email,
+
+        @NotBlank(message = "Senha obrigatória")
+        String senha,
+
+        @NotBlank(message = "Nome obrigatório")
+        String nome
+) {}
